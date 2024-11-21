31d8df3273e368f7f4ca147af98e036c25945859
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-21 20:30:20 +0600

    checking config main

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/Dhar01/Gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Println("Initial config:", cfg)
+
+	err = cfg.SetUser("jane")
+
+	updatedCfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Println("Updated config: ", updatedCfg)
 }
