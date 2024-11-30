f4a1cd0fb609197a2ce15ed32a76360259e20f18
Loknath Dhar <loknathdhar66@gmail.com>
2024-11-30 20:39:30 +0600

    adding user registration

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Dhar01/Gator/commands"
 	"github.com/Dhar01/Gator/handlers"
 	"github.com/Dhar01/Gator/internal/config"
+	"github.com/Dhar01/Gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 var errLessArg = errors.New("not enough arguments provided")
@@ -15,10 +19,20 @@ var errLessArg = errors.New("not enough arguments provided")
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	state := commands.State{
+	dbURL := "postgres://postgres:postgres@localhost:5432/gator"
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
+	state := &commands.State{
+		DB:     dbQueries,
 		Config: &cfg,
 	}
 
@@ -27,6 +41,7 @@ func main() {
 	}
 
 	cmd.Register("login", handlers.HandlerLogin)
+	cmd.Register("register", handlers.HandlerRegister)
 
 	if len(os.Args) < 2 {
 		fmt.Println(errLessArg)
@@ -38,9 +53,10 @@ func main() {
 		Args: os.Args[2:],
 	}
 
-	err = cmd.Run(&state, command)
+	err = cmd.Run(state, command)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("ERROR: %v", err)
 		os.Exit(1)
 	}
+
 }
