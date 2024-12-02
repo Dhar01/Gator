e4b923c7bb29ae289ce4fb43a4673659e552e00a
Loknath Dhar <loknathdhar66@gmail.com>
2024-12-03 03:21:37 +0600

    adding addfeed command and feed table

---
+++ handlers/addfeed.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/Dhar01/Gator/commands"
+	"github.com/Dhar01/Gator/internal/database"
+	"github.com/google/uuid"
+)
+
+var errUsage = errors.New("Usage: %s <name> <feed link>")
+
+func HandlerAddFeed(s *commands.State, cmd commands.Command) error {
+	if len(cmd.Args) < 2 || len(cmd.Args) < 1 {
+		return errUsage
+	}
+
+	username := s.Config.CurrentUserName
+	user, err := s.DB.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("couldn't find user!")
+	}
+
+	name := cmd.Args[0]
+	url := cmd.Args[1]
+
+	data := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	}
+
+	feed, err := s.DB.CreateFeed(context.Background(), data)
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+
+	fmt.Printf("%+v", feed)
+
+	return nil
+}
--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -12,6 +12,65 @@ import (
 	"github.com/google/uuid"
 )
 
+const createFeed = `-- name: CreateFeed :many
+INSERT INTO feeds(id, created_at, updated_at, name, url, user_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6
+)
+RETURNING id, created_at, updated_at, name, url, user_id
+`
+
+type CreateFeedParams struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Name      string
+	Url       string
+	UserID    uuid.UUID
+}
+
+func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) ([]Feed, error) {
+	rows, err := q.db.QueryContext(ctx, createFeed,
+		arg.ID,
+		arg.CreatedAt,
+		arg.UpdatedAt,
+		arg.Name,
+		arg.Url,
+		arg.UserID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Feed
+	for rows.Next() {
+		var i Feed
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.Name,
+			&i.Url,
+			&i.UserID,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const createUser = `-- name: CreateUser :one
 INSERT INTO users(id, created_at, updated_at, name)
 VALUES (
